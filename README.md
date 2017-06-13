dc8435795aa332d0b7fe7c7c93f0f721feff5c12
alexjackhughes <alexjackhughes@gmail.com>
2017-06-13 11:03:25 +0100

    commented code - add basic interactivity

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple Reddit-Style application, fundamentally to help me learn
+more about MEAN technologies, and how they work together in an application.
--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
-    'scope', function($scope){
-        $scope.test = "Hello World!";
+'$scope',
 
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 10},
-            {title: 'post 3', upvotes: 3},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 12}
-        ];
+// Logic of creating posts:
+function($scope){
+  $scope.test = 'Reddit Clone - Powered by Alexander Jack Hughes'; // Title of app
 
-        $scope.addPost = function() {
-            $scope.post.push(title: 'A New Post!', upvotes: 4);
-        }
-    }]);
+  $scope.posts = [ // List of default posts
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+    ];
+
+    // function for creating posts
+    $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') { return; } // Disallows blank posts
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+        });
+        $scope.title='';
+        $scope.link='';
+    };
+
+    // function for upvoting posts
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+    }
+}]);
